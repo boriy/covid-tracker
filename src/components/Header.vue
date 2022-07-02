8c21535285eed829c18fe3f9600ba5f6a7fac4e4
Abdullayev Abdulboriy <boriy9144@gmail.com>
2022-07-02 09:47:00 +0300

    test

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+     <header class="text-center bg-blue-800 text-white p-4 mb-10"> 
+        <div class="text-1xl md:text-3xl font-bold mb-3">
+         <i class="fa fa-viruses"></i> Covid-19 Tracker
+
+       
+        </div>
+
+           <p>
+             API by
+            <a class="text-blue-300 font-italic" target="_blank" href="https://covid19api.com">covid19api.com</a>
+          </p>
+     </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+
+}
+</script>
+
+
+<style scoped>
+
+</style>
